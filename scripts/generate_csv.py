4669e0e4e7c4bc22f23fca70e0d6d5ce4258ada9
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-12-26 22:26:17 -0500

    Generated csv file
    Started to address #3 but I still can't match the inputs.
    @Vyrwn I spent good amount of time to try to match the values based on
    Grasshopper file but I couldn't really figure it out. You can see list
    of the inputs and values in scripts/generate_csv.py
    If you can come up with a clean list of the values, 4 for each, I can
    clean the csv and move forward to the interface.

--- scripts/generate_csv.py
+++ scripts/generate_csv.py
@@ -2,31 +2,6 @@
 import os
 import json
 
-# inputs as key and possible values
-inputs = (
-    {"name": "Area", "min": 1250, "max": 5000, "from": 1250, "step": 3750},
-    {"name": "Aspect_Ratio", "min": 1, "max": 1.5, "from": 1, "step": .5},
-    {"name": "Height", "min": 4, "max": 4, "from": 4, "step": 4},
-    {"name": "Offset", "min": 4, "max": 4, "from": 4, "step": 0},
-    {"name": "FloorCount", "min": 1, "max": 4, "from": 1, "step": 4},
-    {"name": "Orientation", "min": 0, "max": 0, "from": 0, "step": 0},
-    {"name": "Window_Wall_Ratio", "min": 30, "max": 50, "from": 30, "step": 20},
-    {"name": "Window_Height", "min": 2, "max": 2, "from": 2, "step": 0},
-    {"name": "Sill_Height", "min": 0.7, "max": 0.7, "from": 0.7, "step": 0},
-    {"name": "Shading_depth", "min": 0, "max": 1, "from": 1, "step": 1},
-    {"name": "Infiltration", "min": 0.0001, "max": 0.0003, "from": 0.0001, "step": 0.0001},
-    {"name": "R_value_Wall", "min": 2.6, "max": 7, "from": 2.6, "step": 4.4},
-    {"name": "R_value_Roof", "min": 3.5, "max": 7, "from": 3.5, "step": 3.5},
-    {"name": "Glazing_Type", "min": 0, "max": 1, "from": 0, "step": 1},
-    {"name": "Outside_Air_Core", "min": 0.06, "max": 0.06, "from": 0.06, "step": 0},
-    {"name": "Outside_Air_Perimeter", "min": 0.06, "max": 0.3, "from": 0.06, "step": 0.24},
-    {"name": "Internal_Load_Core", "min": 1, "max": 1, "from": 1, "step": 1},
-    {"name": "Internal_Load_Perimeter", "min": 1, "max": 1, "from": 1, "step": 0},
-    {"name": "Comfort_Range", "min": 0.06, "max": 0.06, "from": 0.06, "step": 0},
-    {"name": "HVAC_Systems", "min": 0, "max": 3, "from": 0, "step": 1}
-)
-
-outputs = (u'unmet_cooling_hours', u'eui', u'lighting_power_density', u'daylight_factor', u'heating', u'unmet_heating_hours', u'cooling')
 
 def frange(start, stop, step):
     """Range function for float values."""
@@ -43,17 +18,56 @@ def frange(start, stop, step):
         v += step
     return vs
 
-for i in inputs:
-    print frange(i['min'], i['max'] + i['step'], i['step'])
-# for f in os.listdir('../res'):
-#     # find input values based on file name
-#     for s in f:
-#         print s
-#
-#     with open(os.path.join('../res', f)) as jf:
-#         j = json.load(jf)
-#         for o in outputs:
-#             # read the output Value
-#             print j[o]
+# inputs as key and possible values
+inputs = (
+    {'id': 1, "name": "FloorCount", "min": 1, "max": 4, "from": 1, "step": 4},
+    {'id':2, "name": "Area", "min": 1250, "max": 5000, "from": 1250, "step": 3750},
+    {'id': 3, "name": "Aspect_Ratio", "min": 1, "max": 1.5, "from": 1, "step": .5},
+    {'id': 4, "name": "Offset", "min": 4, "max": 4, "from": 4, "step": 0},
+    {'id': 5, "name": "Orientation", "min": 0, "max": 0, "from": 0, "step": 0},
+    {'id': 6, "name": "Height", "min": 4, "max": 4, "from": 4, "step": 4},
+    {'id': 7, "name": "Window_Wall_Ratio", "min": 30, "max": 50, "from": 30, "step": 20},
+    {'id': 8, "name": "Window_Height", "min": 2, "max": 2, "from": 2, "step": 0},
+    {'id': 9, "name": "Sill_Height", "min": 0.7, "max": 0.7, "from": 0.7, "step": 0},
+    {'id': 10, "name": "Infiltration", "min": 0.0001, "max": 0.0004, "from": 0.0001, "step": 0.0001}, #max is actually 0.0003
+    {'id': 11, "name": "R_value_Wall", "min": 2.6, "max": 7, "from": 2.6, "step": 4.4},
+    {'id': 12, "name": "R_value_Roof", "min": 3.5, "max": 7, "from": 3.5, "step": 3.5},
+    {'id': 13, "name": "Glazing_Type", "min": 0, "max": 1, "from": 0, "step": 1},
+    {'id': 14, "name": "Shading_depth", "min": 0, "max": 1, "from": 1, "step": 1},
+    {'id': 15, "name": "Outside_Air_Core", "min": 0.06, "max": 0.06, "from": 0.06, "step": 0},
+    {'id': 16, "name": "Outside_Air_Perimeter", "min": 0.06, "max": 0.3, "from": 0.06, "step": 0.24},
+    {'id': 17, "name": "Internal_Load_Core", "min": 1, "max": 1, "from": 1, "step": 1},
+    {'id': 18, "name": "Internal_Load_Perimeter", "min": 1, "max": 1, "from": 1, "step": 0},
+    {'id': 18, "name": "Comfort_Range", "min": 0.06, "max": 0.06, "from": 0.06, "step": 0},
+    {'id': 20, "name": "HVAC_Systems", "min": 0, "max": 3, "from": 0, "step": 1}
+)
 
-# write the results as a csv file
+outputs = (u'unmet_cooling_hours', u'eui', u'lighting_power_density',
+           u'daylight_factor', u'heating', u'unmet_heating_hours', u'cooling')
+
+content = [','.join('in:%s' % inp['name'] for inp in inputs) + ',' + \
+         ','.join('out:%s' % out for out in outputs)]
+
+for inp in inputs:
+    inp['values'] = frange(inp['min'], inp['max'] + inp['step'], inp['step'])
+
+def get_input_values(inp, index):
+    try:
+        return str(inp['values'][int(index)])
+    except IndexError:
+        print 'Bug: ', index, inp['name'], inp['values']
+        return str(inp['values'][-1])
+
+for f in os.listdir('../res'):
+    # find input values based on file name
+    v = ','.join(get_input_values(inp, f[count]) for count, inp in enumerate(inputs))
+
+    with open(os.path.join('../res', f)) as jf:
+        d = json.load(jf)
+        o = ','.join(str(d[ot]) for ot in outputs)
+
+    content.append(','.join((v, o)))
+
+# # write the results as a csv file
+with open('results.csv', 'wb') as csvfile:
+    csvfile.write('\n'.join(content))
