5ae390ac851189efcf7ea4a54da2bb94bc8cf9d8
LAPTOP-MB0HU4QI\Mostapha <sadeghipour@gmail.com>
2017-01-05 18:07:17 -0500

    PEP8!

--- scripts/process_results.py
+++ scripts/process_results.py
@@ -5,11 +5,13 @@ import sqlite3
 import json
 import sys
 
+
 def update_progress(progress):
     p = int(progress / 5)
     r = 20 - p
     sys.stdout.write('\r[{0}{1}] {2:.2f}%'.format('|' * p, '.' * r, progress))
     sys.stdout.flush()
+
 
 def sql_query(input_path, input_filename, report_name, table_name, row_name, column_name):
 
@@ -21,7 +23,6 @@ def sql_query(input_path, input_filename, report_name, table_name, row_name, col
 
     con = sqlite3.connect(os.path.join(input_path, input_filename))
 
-    #with con:
     cur = con.cursor()
     val = 0
     sql_ok = 'no'
@@ -42,7 +43,7 @@ def sql_query(input_path, input_filename, report_name, table_name, row_name, col
 
 # Inputs
 input_path = '..\\raw_results'
-output_path='..\\res'
+output_path = '..\\res'
 
 # PROCESS SQL FILES
 input_filenames = tuple(f for f in os.listdir(input_path) if f.endswith(".sql"))
@@ -56,18 +57,18 @@ for count, input_filename in enumerate(input_filenames):
     eui = sql_query(input_path, input_filename,
                     'AnnualBuildingUtilityPerformanceSummary',
                     'Site and Source Energy', 'Total Site Energy',
-                    'Energy Per Total Building Area')  #MJ/m2
+                    'Energy Per Total Building Area')  # MJ/m2
 
     cooling = sql_query(input_path, input_filename,
                         'AnnualBuildingUtilityPerformanceSummary',
-                        'End Uses', 'Cooling', 'Electricity')  #GJ
+                        'End Uses', 'Cooling', 'Electricity')  # GJ
     cooling += sql_query(input_path, input_filename,
                          'AnnualBuildingUtilityPerformanceSummary',
                          'End Uses', 'Cooling', 'District Cooling')
 
     heating = sql_query(input_path, input_filename,
                         'AnnualBuildingUtilityPerformanceSummary',
-                        'End Uses', 'Heating', 'Electricity')  #GJ
+                        'End Uses', 'Heating', 'Electricity')  # GJ
     heating += sql_query(input_path, input_filename,
                          'AnnualBuildingUtilityPerformanceSummary',
                          'End Uses', 'Heating', 'Natural Gas')
@@ -76,7 +77,7 @@ for count, input_filename in enumerate(input_filenames):
                          'End Uses', 'Heating', 'District Heating')
 
     unmet_cooling_hours = sql_query(input_path, input_filename,
-                                    'SystemSummary','Time Setpoint Not Met',
+                                    'SystemSummary', 'Time Setpoint Not Met',
                                     'Facility', 'During Occupied Cooling')
     unmet_heating_hours = sql_query(input_path, input_filename,
                                     'SystemSummary', 'Time Setpoint Not Met',
@@ -87,7 +88,6 @@ for count, input_filename in enumerate(input_filenames):
                                        'Interior Lighting Total',
                                        'Lighting Power Density')
 
-
     # read daylight factor data
     # Based on my understanding this is totally wrong. For now I set all the values
     # to 0.25 until we get it fixed.
@@ -95,14 +95,13 @@ for count, input_filename in enumerate(input_filenames):
 
     # if os.path.exists(os.path.join(input_path, os.path.splitext(input_filename)[0] + '.res')):
     #     rad_data = pd.read_csv(
-            # os.path.join(input_path, os.path.splitext(input_filename)[0] + '.res'),
-            # sep='\t', lineterminator='\n', header=None, usecols=[0])
+    #       os.path.join(input_path, os.path.splitext(input_filename)[0] + '.res'),
+    #       sep='\t', lineterminator='\n', header=None, usecols=[0])
     #     series_size = rad_data.size
     #     dlf = float(rad_data[0][rad_data[0]>0.2].count())/series_size
     # else:
     #     rad_data = None
     #     dlf = 0.25
-
 
     min_eui = min(eui, min_eui)
     max_eui = max(eui, max_eui)
@@ -115,7 +114,7 @@ for count, input_filename in enumerate(input_filenames):
         'unmet_heating_hours': unmet_heating_hours,
         'lighting_power_density': lighting_power_density,
         'daylight_factor': dlf
-        #'sensible heat gain summary'
+        # 'sensible heat gain summary'
     }
 
     update_progress(count * 100 / numOfFiles)
@@ -125,7 +124,6 @@ for count, input_filename in enumerate(input_filenames):
 
     with open(os.path.join(output_path, input_filename[:-4] + '.json'), 'w') as outfile:
         json.dump(case_dict, outfile)
-
 
     # print('')
     # print('EUI: ' + str(eui)
